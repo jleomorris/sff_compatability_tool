35d61c60551aee74e27654e2b019760faa4d657f
James Morris <70540466+jleomorris@users.noreply.github.com>
2020-10-20 02:10:11 +0100

    Update tutorial cards and styling

--- src/components/TutorialCards.js
+++ src/components/TutorialCards.js
@@ -18,24 +18,30 @@ class TutorialCards extends Tool {
   render() {
     return (
       <Container fluid className="tutorial-container">
-        <h2 id="features" ref={(ref) => { this.myRef[0] = ref }}>This tool makes matching components so much easier.</h2>
-        <div className="tutorial-container-text">Check out the automatic features</div>
+        <h2 id="features" ref={(ref) => { this.myRef[0] = ref }}>This tool makes matching components much easier.</h2>
+        <div className="tutorial-container-text">Check out the features</div>
         <Container>
           <Row className="tutorial-card-container">
-            <Col md={3} className="tutorial-card" id="tutorial-card-1">
-              <div className="tutorial-card-title">Filter compatabile parts</div>
-              <div className="tutorial-card-text">Click on a case and comptabile CPU coolers, PSUs and GPUs will be automatically filtered.</div>
-              <FontAwesomeIcon icon={faFilter}/>
+            <Col lg={3} className="tutorial-card" id="tutorial-card-1">
+              <div className="tutorial-card-title">Filter compatible parts</div>
+              <div className="tutorial-card-text">Click on a case and compatible CPU coolers, PSUs and GPUs will be automatically filtered.</div>
+              <div className="icon-background">
+                <FontAwesomeIcon icon={faFilter}/>
+              </div>
             </Col>
-            <Col md={3} className="tutorial-card" id="tutorial-card-2">
+            <Col lg={3} className="tutorial-card" id="tutorial-card-2">
               <div className="tutorial-card-title">Reverse filter according to part</div>
               <div className="tutorial-card-text">Hover over the eye icon to highlight compatible cases.</div>
-              <FontAwesomeIcon icon={faEye}/>
+              <div className="icon-background">
+                <FontAwesomeIcon icon={faEye}/>
+              </div>
             </Col>
-            <Col md={3} className="tutorial-card" id="tutorial-card-3">
+            <Col lg={3} className="tutorial-card" id="tutorial-card-3">
               <div className="tutorial-card-title">Breakdown</div>
               <div className="tutorial-card-text">After selecting a case, cooler, GPU and PSU a breakdown will be shown displaying a detailed spec.</div>
-              <FontAwesomeIcon icon={faFileAlt}/>
+              <div className="icon-background">
+                <FontAwesomeIcon icon={faFileAlt}/>
+              </div>
             </Col>
           </Row>
         </Container>
